b9a2cb78b9a54ce576e572ac657fb050c9390402
dsfgusputra <75104445+dsfgusputra@users.noreply.github.com>
2020-12-28 16:46:03 +0700

    Update 00.TESTING-23122020.ps1

--- 00.TESTING-23122020.ps1
+++ 00.TESTING-23122020.ps1
@@ -56,7 +56,7 @@ function WriteLog{
 
 $branch	     = "TESTING"
 $hostname    = "JHO1DSFNBITPM5"
-$source      = "\\$hostname\d$\TESTBPKB1"
+$source      = "\\$hostname\d$\TESTBPKB"
 $dest        = "D:\BPKB\00.TEST"
 
 $PingResult = ValidateHost -hostname $hostname
